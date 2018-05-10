123b7e4fdda4514d5b03c99c85288ab6d284de5a
nccu104703012 <wade851028@gmail.com>
2018-05-10 23:18:56 +0800

    commit test

---
+++ pepper/mqtt_test/json_test/sensor_pub.py
@@ -0,0 +1,61 @@
+#coding=utf-8
+
+import paho.mqtt.client as mqtt
+import json
+# Define Variables
+MQTT_HOST = "192.168.0.10"
+MQTT_PORT = 1883
+MQTT_KEEPALIVE_INTERVAL = 45
+MQTT_TOPIC = "sensor/test"
+
+MQTT_MSG=json.dumps({"check": "one","token1":  "get token1","petalLength": "4.5","petalWidth":  "1.5"});
+MQTT_MSG2=json.dumps({"check": "two","token2":  "get token2","petalLength": "4.5","petalWidth":  "1.5"});
+
+output = {}
+RShoulderPitch = "RShoulderPitch"
+RShoulderRoll = "RShoulderRoll"
+LShoulderPitch = "LShoulderPitch"
+LShoulderRoll = "LShoulderRoll"
+RElbowYaw = "RElbowYaw"
+RElbowRoll = "RElbowRoll"
+LElbowYaw = "LElbowYaw"
+LElbowRoll = "LElbowRoll"
+
+'''
+RShoulderPitch 上下 -119.5 to 119.5
+RShoulderRoll 側邊開合 -89.5 to -0.5
+LShoulderPitch -119.5 to 119.5
+LShoulderRoll 	0.5 to 89.5
+RElbowYaw 轉動  -119.5 to 119.5
+RElbowRoll 屈伸  0.5 to 89.5
+LElbowYaw 	-119.5 to 119.5
+LElbowRoll	-89.5 to -0.5
+'''
+output['RShoulderPitch'] = RShoulderPitch
+output['RShoulderRoll'] = RShoulderRoll
+output['LShoulderPitch'] = LShoulderPitch
+output['LShoulderRoll'] = LShoulderRoll
+output['RElbowYaw'] = RElbowYaw
+output['RElbowRoll'] = RElbowRoll
+output['LElbowYaw'] = LElbowYaw
+output['LElbowRoll'] = LElbowRoll
+
+output = json.dumps(output)
+
+def on_publish(client, userdata, mid):
+    print "Message Published..."
+
+
+# Initiate MQTT Client
+mqttc = mqtt.Client()
+
+mqttc.on_publish = on_publish
+
+mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+
+while 1 :
+    name = raw_input('Enter input:')
+    if name=="1" :
+        mqttc.publish(MQTT_TOPIC, output)
+    if name=="2" :
+        mqttc.publish(MQTT_TOPIC, MQTT_MSG2)
---
+++ pepper/mqtt_test/json_test/sensor_sub.py
@@ -0,0 +1,34 @@
+import paho.mqtt.client as mqtt
+import json
+# Define Variables
+MQTT_HOST = "192.168.0.10"
+MQTT_PORT = 1883
+MQTT_KEEPALIVE_INTERVAL = 45
+MQTT_TOPIC = "sensor/test"
+
+def on_connect(client, userdata, flags, rc):
+    print "Connected"
+    client.subscribe(MQTT_TOPIC)
+
+def on_message(client, userdata, msg):
+    #print(msg.topic)
+    #print(msg.payload) # <- do you mean this payload = {...} ?
+    payload = json.loads(msg.payload) # you can use json.loads to convert string to json
+    #print(payload['check']) # then you can check the value
+    #if payload['check']=="one":
+    #    print(payload['token1'])
+    #    client.disconnect()
+    print(json.dumps(payload, indent=4))
+    #client.disconnect() # Got message then disconnect
+
+# Initiate MQTT Client
+mqttc = mqtt.Client()
+
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+
+print("end connect")
+mqttc.loop_forever()
+print("end loop")
